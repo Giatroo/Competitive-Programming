ebbb15c8ac1cc07241806796bfd93791ebef0866
Lucas Paiolla Forastiere <giatro@assembly.linux.ime.usp.br>
2019-10-02 07:59:10 -0300

    Praticamente terminado o problema 460C CodeForces

--- 460C.cpp
+++ 460C.cpp
@@ -2,30 +2,34 @@
 using namespace std;
 typedef long long ll;
 
-ll W[112345];
+ll H[112345];
 
 ll binary(ll l, ll r, ll k, ll n){
 	ll m, t;
-	while(l < r - 1){
+	while(l < r-1){
 		m = (l+r)/2;
-		for(ll i = 0, t = 0; i < n; i++)
-			t += (m-W[i]);
-		if(t < m) l = m;
-		else if(t > m) r = m;
- 		else if(t == m) return t;
+		t = 0;
+		for(ll i = 0; i < n; i++){
+			t += (m-H[i]);
+			if(t > k) break;
+		}
+		if(t < k) l = m;
+		else if(t > k) r = m;
+ 		else if(t == k) return m;
 	}
-	return t;
+	m = (l+r)/2;
+	return m;
 }
 
 int main(){
 	ll N, M, W;
 	scanf("%d %d %d", &N, &M, &W);
 	for(int i = 0; i < N; i++)
-		scanf("%d", W+i);
-	sort(W, W+N);
+		scanf("%d", H+i);
+	sort(H, H+N);
 	ll k = M * W;
-	ll min = W[0];
-	ll max = W[N-1] + k;
+	ll min = H[0];
+	ll max = H[N-1] + k;
 	ll p = binary(min, max, k, N);	
 	
 	printf("%d\n", p);
