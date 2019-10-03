cd8668c5b9ff3edcd09e8f9b0b66c0e761062de2
Lucas Paiolla <lucaspaiolla@gmail.com>
2019-10-03 16:47:12 -0300

    Alterado problema 460C

--- 460C.cpp
+++ 460C.cpp
@@ -5,34 +5,36 @@ typedef long long ll;
 ll H[112345];
 
 ll binary(ll l, ll r, ll k, ll n){
-	ll m, t;
-	while(l < r-1){
-		m = (l+r)/2;
+	ll m, t, i;
+	while(l <= r){
+		m = l + (r-l)/2;
 		t = 0;
-		for(ll i = 0; i < n; i++){
+		for(i = 0; i < n; i++){
 			t += (m-H[i]);
-			if(t > k) break;
+			//if(t > k) break;
 		}
-		if(t < k) l = m;
-		else if(t > k) r = m;
- 		else if(t == k) return m;
+		if(t < k) l = m+1;
+		else if(t > k) r = m-1;
+ 		else if(t == k) break;
 	}
-	m = (l+r)/2;
+	m = l + (r-l)/2;
 	return m;
 }
 
 int main(){
 	ll N, M, W;
-	scanf("%d %d %d", &N, &M, &W);
-	for(int i = 0; i < N; i++)
-		scanf("%d", H+i);
+	ll k, p, min, max;
+	ll i;
+	scanf("%lld %lld %lld", &N, &M, &W);
+	for(i = 0; i < N; i++)
+		scanf("%lld", H+i);
+	k = M * W;
 	sort(H, H+N);
-	ll k = M * W;
-	ll min = H[0];
-	ll max = H[N-1] + k;
-	ll p = binary(min, max, k, N);	
+	min = H[0];
+	max = H[N-1] + k;
+	p = binary(min, max, k, N);	
 	
-	printf("%d\n", p);
+	printf("%lld\n", p);
 		
 	return 0;
 }
