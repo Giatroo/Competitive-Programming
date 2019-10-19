430a7dd21a62af654f7b6a17766fb8f16a27a0c7
Lucas Paiolla <lucaspaiolla@gmail.com>
2019-10-19 12:02:56 -0300

    Terminado o problema 1041D

--- 1041D.cpp
+++ 1041D.cpp
@@ -1,69 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define f first
+#define s second
 
 pair<ll, ll> a[212345];
 
-ll verificaPara(ll h, ll n){ //n é o tamanho do array
-  ll max;
-  ll l, r;
-  ll il, ir;
+ll decide(ll h, ll n){
+	ll l, r, il, ir, max, oh;
+	l = r = a[0].f - h; //garante que vamos pegar todos os casos
+	il = ir = 0;
+	max = 0;
+	oh = h;
 
-  il = ir = max = 0;
-  l = a[il].first;
-  r = a[ir].first;
+	while(r < (a[n-1].s + h) || h < 0){ //garante que vamos até o final
+//		printf("h = %lld l = %lld il = %lld r = %lld ir = %lld max = %lld\n", h, l, il, r, ir, max);
+		if(h > 0){ //Mexemos no r até h terminar
+			if(a[ir].f <= r && r <= a[ir].s){
+				r = a[ir].s;
+				ir++;
+			} else {
+				h -= (a[ir].f - r);
+				r = a[ir].f;
+				if(h < 0){
+					r += h;
+					h = 0;
+				}
+			}
+		} else { //Devemos mexer no l
+			if(r - l > max) max = r - l;
+			if(a[il].f <= l && l <= a[il].s) //Nesses casos, temos que a distância é a mesma para qualquer l nessas condições
+			{
+				l = a[il].s + 1; //Colocamos o l fora da região de ar
+				h++; //Ganhamos uma altura
+				il++; //Vamos para a próxima região com o il.
+			} else {
+				ll dl, dr;
+				dl = a[il].f - l; dr = a[ir].f - r;
+				if(dl < dr)
+				{
+					l = a[il].s + 1;
+					il++;
+					h += dl + 1;
+				} else
+				{
+					l += dr + 1;
+					h += dr + 1;
+				}
+				if(r <= l){ h = oh; r = l; }
+			}
+		}
+	}
 
-  printf("n = %lld\n", n);
-
-  while(r < n && l <= r){
-    printf("l = %lld r = %lld h = %lld max = %lld\n", l, r, h, max);
-    if(h >= 0){
-      if(h - (a[ir].second - a[ir].first) < 0){
-        r += h;
-        h = -1;
-      } else {
-        h -= (a[ir].second - a[ir].first);
-        ir++;
-        r = a[ir].first;
-      }
-    } else {
-      if(l < a[il].second){
-        l++;
-        h = 1;
-      } else {
-        il++;
-        l = a[il].first;
-      }
-    }
-    if(max < r - l) max = r - l;
-  }
-
-  printf("l = %lld r = %lld h = %lld max = %lld\n", l, r, h, max);
-
-  return max;
+	if(r - l > max) max = r - l;
+//	printf("h = %lld l = %lld il = %lld r = %lld ir = %lld max = %lld\n", h, l, il, r, ir, max);
+	return max;	
 }
 
-int main()
-{
-  ll i, j;
-  ll n, h;
-  ll p, s;
-  scanf("%lld %lld", &n, &h);
+int main(){
+	ll n, h;
+	ll i;
 
-  p = s = 0;
+	scanf("%lld %lld", &n, &h);
+	
+	for(i = 0; i < n; i++)
+		scanf("%lld %lld", &(a[i].f), &(a[i].s));
 
-  for(i = 0; i < n; i++){
-    scanf("%lld", &p);
-    a[i].first = s;
-    a[i].second = p;
+	a[i].f = a[i-1].s + 2*h;
+	a[i].s = a[i].f + 1;
 
-    scanf("%lld", &s);
-  }
-  a[i].first = s;
-  a[i].second = 212345; //+inf
+	printf("%lld\n", decide(h, n));	
 
-  printf("%lld\n", verificaPara(h, a[i].first+h));
-
-
-  return 0;
+	return 0;
 }
