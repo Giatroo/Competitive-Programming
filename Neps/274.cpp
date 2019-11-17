2f3d06bca17c269771dae5f173ee8b6c0608bda0
Lucas Paiolla <lucaspaiolla@gmail.com>
2019-11-17 12:42:52 -0300

    modificacoes no template

--- Neps/274.cpp
+++ Neps/274.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 typedef long long int ll;
-typedef long double lld;
 #define fora(i, n) for(ll i = 0; i < n; i++)
 #define forai(i, n) for(ll i = 1; i <= n; i++)
 #define ford(i, n) for(ll i = n-1; i >= 0; i--)
@@ -18,7 +17,36 @@ ll max(ll a, ll b)
   return (a > b) ? a : b;
 }
 
+vector<ll> myhash, fila;
+ll N, M;
+ll aux;
+
 int main(int argc, char const *argv[]) {
+  myhash.resize(112345);
+  cin >> N;
+  fora(i, N)
+  {
+    cin >> aux;
+    fila.push_back(aux);
+    myhash[aux] = fila.size() - 1;
+  }
+
+  cin >> M;
+  fora(i, M)
+  {
+    cin >> aux;
+    fila[myhash[aux]] = 0;
+  }
+
+  fora(i, N-1)
+  {
+    if(fila[i])
+      cout << fila[i] << " ";
+  }
+  if(fila[N-1])
+    cout << fila[N-1] << endl;
+  else
+    cout << endl;
 
   return 0;
 }
