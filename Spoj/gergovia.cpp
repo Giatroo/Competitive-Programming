1fd77beaa2c915b42eaa15da6a9825e3b898bcc6
Lucas Paiolla <lucaspaiolla@gmail.com>
2019-11-28 15:52:17 -0300

    Terminado o gergovia

--- Spoj/gergovia.cpp
+++ Spoj/gergovia.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 typedef long long int ll;
 typedef long double lld;
-#define fora(i, n) for(ll i = 0; i < n; i++)
-#define forai(i, n) for(ll i = 1; i <= n; i++)
-#define ford(i, n) for(ll i = n-1; i >= 0; i--)
-#define fordi(i, n) for(ll i = n; i >= 0; i--)
+#define fora(i, n) for (ll i = 0; i < n; i++)
+#define forai(i, n) for (ll i = 1; i <= n; i++)
+#define ford(i, n) for (ll i = n-1; i >= 0; i--)
+#define fordi(i, n) for (ll i = n; i >= 0; i--)
 #define f first
 #define s second
 #define sortvector(v) sort(v.begin(), v.end())
 #define sortvectorby(v, f) sort(v.begin(), v.end(), f)
+#define forita(c) for(auto it = c.begin(); it != c.end(); it++)
+#define pb push_back
+#define mp make_pair
 
 using namespace std;
 
@@ -24,6 +27,7 @@ ll max(ll a, ll b)
 
 ll n;
 ll tot;
+ll cur;
 int a[112345];
 
 void printArray(int n)
@@ -39,25 +43,13 @@ int main(int argc, char const *argv[]) {
     tot = 0;
     fora(i, n) cin >> a[i];
 
-    fora(i, n) {
-      ll j = i+1;
-      //printf("a[%lld] = %d\n", i, a[i]);
-      while (a[i]) {
-        if(a[j] * a[i] < 0){
-          tot += min(abs(a[i]), abs(a[j])) * (j-i);
-          if(abs(a[j]) < abs(a[i]))
-          {
-            a[i] += a[j];
-            a[j] = 0;
-          } else {
-            a[j] += a[i];
-            a[i] = 0;
-          }
-        }
-        j++;
-        //printArray(n);
-      }
+    cur = a[0];
+    forai(i, n-1) {
+        // printf("a[i] = %d cur = %lld tot = %lld\n", a[i], cur, tot);
+        tot += abs(cur);
+        cur += a[i];
     }
+
     cout << tot << endl;
     cin >> n;
   }
