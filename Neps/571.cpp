b6f73b364e5c336508ba3b89d1996ce9dac37b79
Lucas Paiolla <lucaspaiolla@gmail.com>
2020-01-10 12:51:47 -0300

    Beachs commit (neps problems)

---
+++ Neps/571.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+typedef long double lld;
+#define fora(i, n) for (ll i = 0; i < n; i++)
+#define forai(i, n) for (ll i = 1; i <= n; i++)
+#define ford(i, n) for (ll i = n-1; i >= 0; i--)
+#define fordi(i, n) for (ll i = n; i >= 0; i--)
+#define forita(it, c) for(auto it = c.begin(); it != c.end(); it++)
+#define foritd(it, c) for(auto it = c.rbegin(); it != c.rend(); it++)
+#define f first
+#define s second
+#define sortvector(v) sort(v.begin(), v.end())
+#define sortvectorby(v, f) sort(v.begin(), v.end(), f)
+#define pb push_back
+#define mk make_pair
+#define pll pair<ll, ll>
+#define cout1(a) cout << (a)
+#define cout2(a, b) cout << (a) << " " << (b)
+#define cout3(a, b, c) cout << (a) << " " << (b) << " " << (c)
+#define cout1e(a) cout1(a) << endl
+#define cout2e(a, b) cout2(a, b) << endl
+#define cout3e(a, b, c) cout3(a, b, c) << endl
+#define get1(a) cin >> (a)
+#define get2(a, b) cin >> (a) >> (b)
+#define get3(a, b, c) cin >> (a) >> (b) >> (c)
+#define INF LLONG_MAX
+#define M 1000000007
+
+using namespace std;
+
+ll a, b;
+bool c[3];
+
+int main(int argc, char const *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  get2(a, b);
+  c[0] = c[1] = c[2] = false;
+  a %= 3;
+  b %= 3;
+  if (a == b) b++;
+  if (b == 3) b = 0;
+  c[a] = c[b] = true;
+  fora (i, 3) if (!c[i]) cout1e(i);
+
+  return 0;
+}
