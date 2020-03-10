4ac47753d10dfedab7608c4b4eed4ca595d5412d
Lucas Paiolla <lucaspaiolla@gmail.com>
2020-03-10 15:04:27 -0300

    Feitos alguns problemas do vjudge

--- Fridge/347.cpp
+++ Fridge/347.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 typedef long long int ll;
+typedef unsigned long long int ull;
 typedef long double lld;
 #define fora(i, n) for (ll i = 0; i < n; i++)
 #define forai(i, n) for (ll i = 1; i <= n; i++)
@@ -23,57 +24,54 @@ typedef long double lld;
 #define get1(a) cin >> (a)
 #define get2(a, b) cin >> (a) >> (b)
 #define get3(a, b, c) cin >> (a) >> (b) >> (c)
+#define get4(a, b, c, d) cin >> (a) >> (b) >> (c) >> (d)
 #define INF LLONG_MAX
+#define MINF LLONG_MIN
 #define M 1000000007
 
 using namespace std;
 
-ll n, q;
-ll o, a, b, k;
-ll arr[112345];
-ll t[412345];
-ll lz[412345];
+ll n, q, k, a, b, o;
+ll seg[412345], lz[412345], arr[112345];
 
 void build(ll l, ll r, ll i) {
-  if (l == r) t[i] = arr[l];
+  if (l == r)
+    seg[i] = arr[l];
   else {
     ll m = l + (r-l)/2;
     build(l, m, 2*i);
     build(m+1, r, 2*i+1);
-    t[i] = t[2*i] + t[2*i+1];
+    seg[i] = seg[2*i] + seg[2*i+1];
   }
   lz[i] = 0;
 }
 
 void push(ll l, ll r, ll i) {
-  if (lz[i]) {
-    t[i] = lz[i] * (r-l+1);
-    if (l != r) {
-      lz[2*i] = lz[i];
-      lz[2*i+1] = lz[i];
-    }
+  if (lz[i] != 0) {
+    seg[i] = lz[i]*(r-l+1);
+    if (l != r)
+      lz[2*i] = lz[2*i+1] = lz[i];
     lz[i] = 0;
   }
 }
 
 void update(ll l, ll r, ll i, ll ql, ll qr, ll x) {
   push(l, r, i);
-  if (qr < l || r < ql) return;
+  if (ql > r || qr < l) return;
   if (ql <= l && r <= qr) {
-    lz[i] = x;
-    push(l, r, i);
+    lz[i] = x; push(l, r, i);
   } else {
     ll m = l + (r-l)/2;
     update(l, m, 2*i, ql, qr, x);
     update(m+1, r, 2*i+1, ql, qr, x);
-    t[i] = t[2*i] + t[2*i+1];
+    seg[i] = seg[2*i] + seg[2*i+1];
   }
 }
 
-ll query(ll l, ll r, ll i, ll ql, ll qr) {
+int query(ll l, ll r, ll i, ll ql, ll qr) {
   push(l, r, i);
-  if (ql <= l && r <= qr) return t[i];
-  if (qr < l || r < ql) return 0;
+  if (r < ql || qr < l) return 0;
+  if (ql <= l && r <= qr) return seg[i];
   ll m = l + (r-l)/2;
   return query(l, m, 2*i, ql, qr) + query(m+1, r, 2*i+1, ql, qr);
 }
@@ -85,15 +83,28 @@ int main(int argc, char const *argv[]) {
   get2(n, q);
   forai (i, n) get1(arr[i]);
   build(1, n, 1);
+
+  // cout << endl;
+  // fora (i, 4*n) {
+  //   cout3e(i, seg[i], lz[i]);
+  // }
+  // cout << endl;
+
   fora (i, q) {
-    get1(o);
+    cin >> o;
     if (o == 1) {
-      get3(a, b, k);
+      cin >> a >> b >> k;
       update(1, n, 1, a, b, k);
     } else {
-      get2(a, b);
-      cout1e(query(1, n, 1, a, b));
+      cin >> a >> b;
+      cout << query(1, n, 1, a, b) << endl;
     }
+
+    // cout << endl;
+    // fora (i, 4*n) {
+    //   cout3e(i, seg[i], lz[i]);
+    // }
+    // cout << endl;
   }
 
   return 0;
