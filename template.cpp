41bde8e0dee0eff3cf5e1dc5441df5476ccc4b18
Lucas Paiolla <lucaspaiolla@gmail.com>
2020-03-17 09:49:29 -0300

    Template modified

--- template.cpp
+++ template.cpp
@@ -1,38 +1,44 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 typedef long long int ll;
 typedef unsigned long long int ull;
 typedef long double lld;
-typedef std::pair<ll, ll> pll;
-#define fora(i, n) for (ll i = 0; i < n; i++)
-#define forai(i, n) for (ll i = 1; i <= n; i++)
-#define ford(i, n) for (ll i = n-1; i >= 0; i--)
-#define fordi(i, n) for (ll i = n; i >= 0; i--)
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+#define fr(i, n) for (ll i = 0; i < n; i++)
+#define frr(i, n) for (ll i = 1; i <= n; i++)
+#define frd(i, n) for (ll i = n; i >= 0; i--)
 #define forita(it, c) for(auto it = c.begin(); it != c.end(); it++)
-#define foritd(it, c) for(auto it = c.rbegin(); it != c.rend(); it++)
+
 #define sortvector(v) sort(v.begin(), v.end())
 #define sortvectorby(v, f) sort(v.begin(), v.end(), f)
+
 #define pb push_back
 #define mk make_pair
 #define f first
 #define s second
+
 #define cout1e(a) cout << (a) << endl
 #define cout2e(a, b) cout << (a) << " " << (b) << endl
 #define cout3e(a, b, c) cout << (a) << " " << (b) << " " << (c) << endl
 #define cout4e(a, b, c, d) cout << (a) << " " << (b) << " " << (c) << " " << (d) << endl
+#define debug(x) cout << #x << " = " << x << endl
 #define get1(a) cin >> (a)
 #define get2(a, b) cin >> (a) >> (b)
 #define get3(a, b, c) cin >> (a) >> (b) >> (c)
 #define get4(a, b, c, d) cin >> (a) >> (b) >> (c) >> (d)
-#define INF LLONG_MAX
-#define MINF LLONG_MIN
-#define M 1000000007
-using namespace std;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f;
+const ll M = 1000000007;
 // ===================================================== //
 
 
-int main(int argc, char const *argv[]) {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+int main(int argc, char const *argv[]) { fastio;
 
   return 0;
 }
