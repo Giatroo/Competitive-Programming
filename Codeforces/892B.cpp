aa541ace14198ee4cb123d2c4ee9bd85096a40b7
Lucas Paiolla <lucaspaiolla@gmail.com>
2020-04-02 18:48:01 -0300

    two pointers problem

---
+++ Codeforces/892B.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double lld;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+#define fr(i, n) for (ll i = 0; i < n; i++)
+#define frr(i, n) for (ll i = 1; i <= n; i++)
+#define frd(i, n) for (ll i = n; i >= 0; i--)
+#define forita(it, c) for(auto it = c.begin(); it != c.end(); it++)
+
+#define sortvector(v) sort(v.begin(), v.end())
+#define sortvectorby(v, f) sort(v.begin(), v.end(), f)
+
+#define pb push_back
+#define mk make_pair
+#define f first
+#define s second
+
+#define cout1e(a) cout << (a) << endl
+#define cout2e(a, b) cout << (a) << " " << (b) << endl
+#define cout3e(a, b, c) cout << (a) << " " << (b) << " " << (c) << endl
+#define cout4e(a, b, c, d) cout << (a) << " " << (b) << " " << (c) << " " << (d) << endl
+#define debug(x) cout << #x << " = " << x << endl
+#define get1(a) cin >> (a)
+#define get2(a, b) cin >> (a) >> (b)
+#define get3(a, b, c) cin >> (a) >> (b) >> (c)
+#define get4(a, b, c, d) cin >> (a) >> (b) >> (c) >> (d)
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f;
+const ll M = 1000000007;
+// ===================================================== //
+
+bool m[1123456];
+int iL[1123456];
+int L[1123456];
+int n, ans;
+
+int main(int argc, char const *argv[]) { fastio;
+	get1(n);
+	frr (i, n) { get1(L[i]); iL[i] = i - L[i]; m[i] = false; }
+
+	int i, j; // 2 pointers
+	i = j = n;
+	
+	while (i > 0) {
+		if (!m[i]) ans++;
+		
+		while (j > 0 && j > iL[i]) {
+			j--;
+			m[j] = true;
+		}	
+		i--;
+	}
+
+	cout1e(ans);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
